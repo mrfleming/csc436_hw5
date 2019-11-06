ad99ed0dccd9da378d56230153064429acd8b6f2
Eric <ericfleming8@gmail.com>
2019-11-06 13:48:20 -0600

    updated module imports for form input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 // import { HttpClientModule } from '@angular/http';
 
 import { UsersService } from './user/users.service';
@@ -26,14 +28,15 @@ import { UserProfileComponent } from './user-profile/user-profile.component';
 
 // Firebase stuff
 import { AngularFireModule } from '@angular/fire';
-
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireStorageModule } from '@angular/fire/storage';
 import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
 
 import { environment } from '../environments/environment';
 
 import { AuthService } from './auth/auth.service';
+import { EmailService } from './auth/email.service';
 import { AuthGuard } from './auth/auth-guard.guard';
 
 
@@ -63,15 +66,18 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot(routes),
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFirestoreModule, // imports firebase/firestore, only needed for database features
     AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
-    AngularFireStorageModule // imports firebase/storage only needed for storage features
+    AngularFireStorageModule, // imports firebase/storage only needed for storage features
+    AngularFireDatabaseModule
   ],
   providers: [
-    MessagesService, ThreadsService, UsersService, AuthService
+    MessagesService, ThreadsService, UsersService, AuthService, EmailService
   ],
 
   bootstrap: [AppComponent]
